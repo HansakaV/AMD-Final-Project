0b4aa37bd29dbbfbf86327d7afdabcab30d5055a
HansakaV <maheshhansaka628@gmail.com>
2025-09-15 20:02:23 +0530

    apk build added

--- app.config.js
+++ app.config.js
@@ -1,5 +1,15 @@
 export default {
-  name: "SriLanka Secrets",
-  slug: "sri-lanka-secrets",
-  version: "1.0.0",
-}
+  expo: {
+    name: "SriLanka Secrets",
+    slug: "sri-lanka-secrets",
+    version: "1.0.0",
+    android: {
+      package: "com.hasa007.srilankasecrets"
+    },
+    extra: {
+      eas: {
+        projectId: "a812186d-faa0-479a-9d6f-940422cff342"
+      }
+    }
+  }
+};
